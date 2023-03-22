6ed54120c3c7b2e64cdba83daa5f278b995c324b
Javiera Fortunato <javiera.fortunato@perficient.com>
2023-03-22 12:26:02 -0300

    Reviewing arrays

---
+++ arrays.ts
@@ -0,0 +1,8 @@
+// type inference define this array as an array of strings ( string[] )
+let hobbies = ["Sports", "Cooking"];
+
+// We can pre-define the type of array:
+let favoriteActivities: string[];
+
+// If we need a flexible array, we can define type as any:
+let allTypeAcceptedArray: any[];
