db8e83b5c1f4cb28609e56ed3b287e035d5b1d89
Javiera Fortunato <javiera.fortunato@perficient.com>
2023-03-26 12:18:53 -0300

    namespaces

---
+++ 28.declaringANamespace.ts
@@ -0,0 +1,8 @@
+//We can create a namespace to add whatever we want,
+//for example a couple of interfaces
+//(We export them so we can use it in the next file)
+
+namespace app {
+  export interface MyFirstInterface {}
+  export interface MySecondInterface {}
+}
---
+++ 29.importingTheNamespace.ts
@@ -0,0 +1,7 @@
+//Now we use this special TS syntax to reference the last file:
+/// <reference path="28.declaringANamespace.ts" />
+
+//And we call the namespace equal so we can use the interfaces:
+namespace app {
+  //Here I can use the interfaces of file n° 28
+}
